91246a6a932af4e36c0618566cdde3c2a6d5c700
Fireye <codekai16@gmail.com>
2025-07-14 20:37:33 -0700

    Waybar update and also foot init

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,18 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+
+vim.api.nvim_create_autocmd({ "VimEnter", "VimResume" }, {
+	callback = function()
+		vim.cmd(
+			[[set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175]]
+		)
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
+	callback = function()
+		vim.cmd([[set guicursor=a:hor3-blinkwait500-blinkoff500-blinkon500]])
+	end,
+})
